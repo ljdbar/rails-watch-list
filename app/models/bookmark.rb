23b674229ef22a7720b039400d1463271eeb7cae
louis barlas <louisjdbarlas@gmail.com>
2022-08-25 18:00:55 +0100

    controllers added, error code to fix

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,16 +2,17 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum: 6 }
+  validates :movie, uniqueness: { scope: :list }
   # add_index :unique_pair, %i[movie_id list_id], unique: true
-  validate :validate_users_are_unique
+  # validate :validate_users_are_unique
 
-  private
+  # private
 
-  def validate_users_are_unique
-    if self.class.where(movie_id: movie_id, list_id: list_id)
-                .or(self.class.where(movie_id: list_id, list_id: movie_id))
-                .exists?
-      errors.add(:base, 'User 1 and User 2 combination exists!')
-    end
-  end
+  # def validate_users_are_unique
+  #   if self.class.where(movie_id: movie_id, list_id: list_id)
+  #               .or(self.class.where(movie_id: list_id, list_id: movie_id))
+  #               .exists?
+  #     errors.add(:base, 'Movie id and List id combination exists!')
+  #   end
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :movies # do
+  #    resources :bookmarks, only: %i[create]
+  # end
+  resources :lists do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
